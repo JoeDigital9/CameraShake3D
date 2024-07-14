c079a3b040154c266eaba5a9084a571eca58bbe1
Joseph Torbett <joseph@reli.sh>
2024-07-14 02:07:26 -0400

    bring camera back to origin position (wip)

--- CameraShake/CameraShake.gd
+++ CameraShake/CameraShake.gd
@@ -6,23 +6,35 @@ var camera : Camera3D
 var strength : float = 1
 var decay : float = 1
 var magnitude : float = 1
+var force : float = 0
+var range : float = 1
+
+var origin : Vector3
+var offset : = Vector2(1, 1)
 
 var isShaking = false
-var offset = Vector2(1, 1)
-var baseForce : float = 1
-var force : float = 0
+var isAtOrigin = true
 
 var _rng : RandomNumberGenerator = RandomNumberGenerator.new()
 
 func _init(pCamera):
 	camera = pCamera
+	origin = camera.position
 
 func _update(delta):
 	if force > 0:
+		# applying shake force
 		force = max(force - (decay) * delta, 0)
+		range = max(range - (decay) * delta, 0)
 		shake()
 	else:
+		# clearing shake state
 		isShaking = false
+		pass
+	
+	# setting camera back to origin pos
+	if isShaking == false:
+		return_to_origin(delta)
 		pass
 	pass
 
@@ -33,19 +45,23 @@ func _start_shake(pMagnitude:float, pDecay:float):
 	decay = pDecay
 	magnitude = pMagnitude
 
+	range = 1
+	force = 1
+	isAtOrigin = false
 	isShaking = true
-	force = baseForce
 
 func shake():
 	if !isShaking: 
 		return
-	
+
 	var amount = force * strength
-	var offsetX = offset.x * amount * _rng.randf_range(-1, 1)
-	var offsetY = offset.y * amount * _rng.randf_range(-1, 1)
+	var offsetX = offset.x * amount * _rng.randf_range(-range, range)
+	var offsetY = offset.y * amount * _rng.randf_range(-range, range)
 
 	# Todo: Add rotational shake
 
-	# Todo: Force the camera to return to its origin position
-
 	camera.position += Vector3(offsetX * magnitude, offsetY * magnitude, 0) # shake y axis
+
+func return_to_origin(delta):
+	camera.position = lerp(camera.position, origin, delta * 2)
+	pass
