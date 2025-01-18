a3c0273da1cfec07cbab8904d1651578523a3048
Joseph Torbett <josephtorbett@icloud.com>
2025-01-18 02:17:53 -0500

    import stuff

--- addons/CameraShake3D/CameraShake3D/CameraShake3DNode.gd
+++ addons/CameraShake3D/CameraShake3D/CameraShake3DNode.gd
@@ -3,15 +3,15 @@ class_name CameraShake3DNode
 
 # Autoload me or drag me into your scene
 
-@export var camera : Camera3D
+@export var camera: Camera3D
 
 @export_group("Manual Settings")
-@export var decay : float = 1
-@export var magnitude : float = 1
+@export var decay: float = 1
+@export var magnitude: float = 1
 
-var cameraShake : CameraShake3D = null
+var cameraShake: CameraShake3D = null
 
-var initComplete : bool = false
+var initComplete: bool = false
 
 func _ready():
 	if camera == null:
